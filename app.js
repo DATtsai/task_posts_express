cdf88e32a1b280703833e66a571ab62c3f435bee
DAT <dat-1293@hotmail.com>
2022-06-04 19:55:00 +0800

    fix. errorhandle and modify posts route

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/', postsRouter);
+app.use('/posts', postsRouter);
 app.use('/upload', uploadRouter);
 
 // catch 404 and forward to error handler
